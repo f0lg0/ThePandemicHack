c2a953c765f386eed51472bc36efe3d367b9c012
f0lg0 <folgonileonardo@gmail.com>
2020-06-06 12:33:20 +0200

    updated with results chart and organized the public folder

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
-let express = require('express');
-let app = express();
-let path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
+app.use(express.static('public/charts'));
+app.use(express.static('public/data'));
+
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/results', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/charts/results.html'));
 });
 
 app.listen(8080);
