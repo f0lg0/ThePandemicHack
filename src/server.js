7939b5535ac44c1c025b9c0cf270568993099af3
f0lg0 <folgonileonardo@gmail.com>
2020-06-10 17:05:46 +0200

    added page to plot data from custom file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.use(express.static('public'));
 app.use(express.static('public/charts'));
@@ -14,6 +15,10 @@ app.get('/', function(req, res) {
 
 app.get('/results', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/charts/results.html'));
+});
+
+app.get('/upload', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/upload.html'));
 });
 
 app.listen(8080);
